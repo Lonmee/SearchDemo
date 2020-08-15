9328bea1858d70e05c11f408611f348ef11beabe
Lonmee <Lonmee@126.com>
2020-08-15 17:05:58 +0800

    xc

--- SearchDemo/Models/Remote.swift
+++ SearchDemo/Models/Remote.swift
@@ -8,13 +8,12 @@
 
 import Foundation
 
-func searchReq(kw: String) -> void {
-    HTTP.GET("https://google.com") { response in
-        if let err = response.error {
-            print("error: \(err.localizedDescription)")
-            return //also notify app of failure as needed
-        }
-        print("opt finished: \(response.description)")
-        //print("data is: \(response.data)") access the response of the data with response.data
+func searchReq(kw: String) -> Void {
+//    HTTP.GET("https://google.com") { response in
+//        if let err = response.error {
+//            print("error: \(err.localizedDescription)")
+//            return //also notify app of failure as needed
+//        }
+//        print("opt finished: \(response.description)")
     }
 }
