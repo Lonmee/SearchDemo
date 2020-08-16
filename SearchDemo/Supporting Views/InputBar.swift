463de2d0578c53df9774d43b084985bd9f7bc3f9
Lonmee <Lonmee@126.com>
2020-08-16 13:17:44 +0800

    input

--- SearchDemo/Supporting Views/InputBar.swift
+++ SearchDemo/Supporting Views/InputBar.swift
@@ -16,32 +16,34 @@ struct InputBar: View {
     
     var body: some View {
         ZStack {
+            // MARK: bg
             RoundedRectangle(cornerRadius: 10, style: .circular)
-                .fill(Color.gray)
-                .opacity(0.1)
-                .frame(height: 38, alignment: .center)
+                .fill(Color.init(red: 244 / 255, green: 244 / 255, blue: 244 / 255))
+                .frame(height: 38)
+            
+            // MARK: input area
             HStack {
+                // MARK: btn magnifying
                 if (keyword.isEmpty) {
                     Image(systemName: "magnifyingglass")
                         .opacity(0.3)
                         .imageScale(.medium)
                         .accessibility(label: Text("Search icon"))
                 }
-                
+                // MARK: input field
                 TextField("Tap here to search", text: Binding<String>(
                 get: { self.keyword },
                 set: {
                     self.keyword = $0
                     reqSearch(keyword: self.keyword, goodsData: self.goodsData)
                 }),
+                // MARK: focus handler
                 onEditingChanged: { editing in
                     withAnimation(.easeInOut(duration: 0.4)) {
                         self.editing = editing
                     }
-                }) {
-                    reqSearch(keyword: self.keyword, goodsData: self.goodsData)
-                }
-                
+                })
+                // MARK: btn clear
                 if (!keyword.isEmpty) {
                     Image(systemName: "clear")
                         .opacity(0.3)
